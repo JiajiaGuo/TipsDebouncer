53ccfa5bf22120ade68364764a6f79b01b09fa56
draw.gu <draw.gu@ecovacs.com>
2019-05-09 16:43:20 +0800

    first commit

---
+++ app/src/main/java/com/ecovacs/drawgu/tipsdebouncer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ecovacs.drawgu.tipsdebouncer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
